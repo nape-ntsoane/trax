c7bfc18656d053d22fb833293a24ee6ca451df29
Nape <napentsoane.dev@gmail.com>
2026-01-15 18:28:15 +0200

    feat(core): implement robust logger

--- backend/app/core/logging.py
+++ backend/app/core/logging.py
@@ -1,9 +1,25 @@
 import logging
-from config import settings
+import sys
+from app.core.config import settings
 
 
-def setup_logging():
-    logging.basicConfig(
-        level=settings.LOGGING_LEVEL,
-        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-    )
+logger = logging.getLogger() # root logger
+LOG_FORMAT = "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+
+formatter = logging.Formatter(
+    fmt=LOG_FORMAT
+)
+
+#handlers
+# stream_handler = logging.StreamHandler(stream=sys.stdout)
+file_handler = logging.FileHandler(filename=settings.LOGGING_FILE)
+
+# set the formats
+# stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# add handlers to the root logger
+# logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+logger.setLevel(settings.LOGGING_LEVEL)
