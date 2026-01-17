a7aff962a34388b648f352ca31e6466a2a19f473
Nape <napentsoane.dev@gmail.com>
2026-01-17 10:09:55 +0200

    fix(auth): hide sensetivce user fields from client

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,14 +1,17 @@
 from fastapi_users import schemas
 import uuid
-from pydantic import EmailStr
+from pydantic import EmailStr, Field
 
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    pass
+    is_active: bool = Field(default=True, exclude=True)
+    is_superuser: bool = Field(default=False, exclude=True)
+    is_verified: bool = Field(default=False, exclude=True)
 
 class UserCreate(schemas.CreateUpdateDictModel):
     email: EmailStr
     password: str
 
-class UserUpdate(schemas.BaseUserUpdate):
-    pass
+class UserUpdate(schemas.CreateUpdateDictModel):
+    password: str | None = None
+    email: EmailStr | None = None
