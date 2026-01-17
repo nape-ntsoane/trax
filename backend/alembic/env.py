bdda86d3a378dd3e944ee6f60612e93be1bca8ae
Nape <napentsoane.dev@gmail.com>
2026-01-17 14:24:37 +0200

    refactor(core:db): make database uri retrieval dynamic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -14,7 +14,7 @@ from app.core.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", str(settings.SQLALCHEMY_DATABASE_URI))
+config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URI))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -34,7 +34,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
-    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    ENVIRONMENT: Literal["local", "testing", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
@@ -65,6 +65,17 @@ class Settings(BaseSettings):
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
         )
+
+    PRODUCTION_DATABASE_URI: str | None = None
+
+    @computed_field
+    @property
+    def DATABASE_URI(self) -> str:
+        if self.ENVIRONMENT == "production":
+            return self.PRODUCTION_DATABASE_URI
+        elif self.ENVIRONMENT == "test":
+            return self.SQLALCHEMY_DATABASE_URI
+        return self.SQLALCHEMY_DATABASE_URI
 
     LOGGING_LEVEL: str = "INFO"
     LOGGING_FILE: str = "app.log"
