cc983bddc020ece78d56b9d3bd677cefac058bfb
Nape <napentsoane.dev@gmail.com>
2026-01-17 14:11:45 +0200

    fix(front:core): redo environment variable access

--- frontend/src/config/app.ts
+++ frontend/src/config/app.ts
@@ -1,4 +1,6 @@
 export const appConfig = {
   environment: process.env.NEXT_PUBLIC_APP_ENV || "dev",
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1",
+  apiUrl: (typeof window === "undefined" && process.env.INTERNAL_API_URL)
+    ? process.env.INTERNAL_API_URL
+    : (process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1"),
 }
